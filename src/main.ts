69e228524904d0f4257c171c5750f97c91d56e88
chijioke <superkingsely@gmail.com>
2024-04-20 12:30:20 -0700

    okay

--- src/main.ts
+++ src/main.ts
@@ -17,61 +17,67 @@ const data:Mydata[]=[
     }
 ]
 
-localStorage.setItem('object',JSON.stringify(data))
-const getObj:any=localStorage.getItem('object')
-const retrive_data:any=JSON.parse(getObj)
+
+
+let Editbtn:any=''
 
 const readAll=()=>{
-    // tbody
-    const table_data:any=document.querySelector('.tableBody')
+    localStorage.setItem('object',JSON.stringify(data))
+    let getObj:any=localStorage.getItem('object')
+    let retrive_data:any=JSON.parse(getObj)
+        // tbody
+        const table_data:any=document.querySelector('.tableBody')
 
-    // tbody child variable
-    let element='';
-    console.log(retrive_data.length )
+        // tbody child variable
+        let element='';
+        console.log(retrive_data.length )
 
-    if(retrive_data.length >=1){
+        if(retrive_data.length >=1){
 
-        retrive_data.map((record:any)=>{
-            element += `
-               <tr>
-                   <td>${record.name}
-                   </td>
-                   <td>
-                       ${record.email}
-                   </td>
-                   <td>
-                       <button class='editBtn' id=${record.id}>Edit+</button>
-                       <button class='deleteBtn' onclick=deleteBtn(${record.id})>Delete</button>
-                   </td>
-               </tr>
-           `
-        //    onclick=editBtn(${record.id},this)
-        //    onclick=edit_btn(${record.id},this)
+            retrive_data.map((record:any)=>{
+                element += `
+                <tr>
+                    <td>${record.name}
+                    </td>
+                    <td>
+                        ${record.email}
+                    </td>
+                    <td>
+                        <button class='editBtn' id=${record.id}>Edit+</button>
+                        <button class='deleteBtn' onclick=deleteBtn(${record.id})>Delete</button>
+                    </td>
+                </tr>
+            `
+            //    onclick=editBtn(${record.id},this)
+            //    onclick=edit_btn(${record.id},this)
 
-        })
-        table_data.innerHTML=element;
-    }else{
-        // let empty=document.querySelector('.empty')
-        // table_data.appendChild(empty)
-        // empty?.classList.add('show')
-        
-        element=`
-        <tr class="empty">
-        <td colspan="3" height="200px">YOUR DATA IS EMTY</td>
-        </tr>
-        `
-        table_data.innerHTML=element;
+            })
+            table_data.innerHTML=element;
+        }else{
+            // let empty=document.querySelector('.empty')
+            // table_data.appendChild(empty)
+            // empty?.classList.add('show')
+            
+            element=`
+            <tr class="empty">
+            <td colspan="3" height="200px">YOUR DATA IS EMTY</td>
+            </tr>
+            `
+            table_data.innerHTML=element;
+        }
+
+         Editbtn=document.querySelectorAll('.editBtn')
+         Editbtn.forEach((btn:any)=>{
+             btn.addEventListener('click',(e:any)=>{
+                 let recordID= e.target.getAttribute('id')
+                 edit_btn(+recordID,e);
+             })
+         })
     }
-
-     const Editbtn=document.querySelectorAll('.editBtn')
-
-     Editbtn.forEach((btn:any)=>{
-        btn.addEventListener('click',(e:any)=>{
-            let recordID= e.target.getAttribute('id')
-            edit_btn(+recordID,e);
-        })
-     })
-}
+    //  Editbtn=document.querySelectorAll('.editBtn')
+    // console.log('edit=',Editbtn)
+    
+    // Editbtn=readAll();
 
 
 
@@ -88,13 +94,16 @@ const readAll=()=>{
 
         let obj=data.find((rec:any)=>{
             console.log('ok',rec.id,id)
-            rec.id===id;
+            return rec.id===id;
         })
-        console.log(obj,retrive_data[0].id,id,data)
+        // console.log(obj,retrive_data[0].id,id,data)
         const inputName:any=document.querySelector('.unname');
         inputName.value=obj?.name;
         const inputEmail:any=document.querySelector('.unemail');
         inputEmail.value=obj?.email;
+        const inputID:any=document.querySelector('#id');
+        inputID.value=obj?.id;
+        // console.log('see',inputID.value)
 
         
         }else if(e.target.innerText==='Edit-'){
@@ -108,6 +117,28 @@ const readAll=()=>{
 
     }
 
+    const update=()=>{
+
+        const clientID:any=document.querySelector('#id')
+        const id= +clientID.value
+        const namesel:any=document.querySelector('.unname')
+        const name=namesel.value
+        const emailsel:any=document.querySelector('.unemail')
+        const email=emailsel.value
+        const index=data.findIndex((rec:any)=>rec.id===id)
+        data[index]={id,name,email}
+        console.log(data[index],data)
+
+
+        // localStorage.setItem('object',JSON.stringify(data))
+        // let getObj:any=localStorage.getItem('object')
+        // let  retrive_data:any=JSON.parse(getObj)
+        readAll();
+        const updateDiv:any=document.querySelector('.update-form')
+        updateDiv.classList.remove('add')
+
+        // edit_btn(id,e);
+    }
 
 
 
